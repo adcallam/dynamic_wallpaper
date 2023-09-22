610d528afbdb3a4d8ab6e01f47fb4a39c8c7aa3f
Andrew Callam <adcallam@uwaterloo.ca>
2023-09-22 18:45:00 -0400

    test

--- dynamic_wallpaper_changer.py
+++ dynamic_wallpaper_changer.py
@@ -3,23 +3,28 @@ import subprocess
 from datetime import datetime
 import calendar
 from suntime import Sun
+import sys
 
-change_based_on_time = True
+DYNAMIC_BRIGHTNESS = False
+VOLUME_ENABLED = False
+PROP_UPDATE_RATE = 900 # Property refresh rate for wallpaper in seconds (such as brightness)
+WP_PATH_PREFIX = "C:\\Users\\andre\\OneDrive\\Pictures\\Wallpapers\\"
+LIVELY_EXE_PATH = "C:\\Users\\andre\\AppData\\Local\\Programs\\Lively Wallpaper\\Lively.exe"
 
-def set_wallpaper(path, monitor):
-    runCommand(['setwp', f'--monitor={monitor}', f'--file={path}'])
+def set_wp(path):
+    run_command(['setwp', f'--monitor={1}', f'--file={path}'])
     return
 
-def set_volume(volume_level):
+def set_vol(volume_level):
     #volume_level should be 0-100
-    runCommand(['volume', f'--volume={volume_level}'])
+    run_command(['volume', f'--volume={volume_level}'])
     return
 
-def changeProperty(name, value, monitor):
-    runCommand(['setprop', f'--monitor={monitor}', f'--property={name}={value}'])
+def change_property(name, value):
+    run_command(['setprop', f'--monitor={1}', f'--property={name}={value}'])
     return
 
-def runCommand(args):
+def run_command(args):
     si = subprocess.STARTUPINFO()
     si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
     subprocess.run(['Livelycu'] + args, startupinfo=si)
@@ -61,7 +66,7 @@ def get_current_season():
 
     return season
 
-def brightness_level():
+def calc_new_brightness():
     MAX_BRIGHTNESS = 10
     MIN_BRIGHTNESS = -20
     BRIGHTNESS_RANGE = MAX_BRIGHTNESS-MIN_BRIGHTNESS
@@ -93,73 +98,71 @@ def brightness_level():
     return brightness
 
 # Select wallpaper based on season
-def select_wallpaper(season):
+def select_wp(season):
     
     if season == "spring":
-        wallpaper_file = "treehouse-spring-morning-desktop-wallpaperwaifu.com.mp4"
-        wallpaper_volume = 50
+        wp_file = "treehouse-spring-morning-desktop-wallpaperwaifu.com.mp4"
+        wp_vol = 50
     elif season == "summer":
-        wallpaper_file = "treehouse-summer-rain-desktop-wallpaperwaifu.com.mp4"
-        wallpaper_volume = 70
+        wp_file = "treehouse-summer-rain-desktop-wallpaperwaifu.com.mp4"
+        wp_vol = 70
     elif season == "fall":
-        wallpaper_file = "treehouse_autumn.mp4"
-        wallpaper_volume = 100
+        wp_file = "treehouse_autumn.mp4"
+        wp_vol = 100
     elif season == "winter":
-        wallpaper_file = "treehouse_winter_sound.mp4"
-        wallpaper_volume = 90
+        wp_file = "treehouse_winter_sound.mp4"
+        wp_vol = 90
     elif season == "christmas":
-        wallpaper_file = "treehouse-winter-holidays-desktop-wallpaperwaifu.com.mp4"
-        wallpaper_volume = 100
+        wp_file = "treehouse-winter-holidays-desktop-wallpaperwaifu.com.mp4"
+        wp_vol = 100
     elif season == "halloween":
-        wallpaper_file = "treehouse_halloween.mp4"
-        wallpaper_volume = 70
+        wp_file = "treehouse_halloween.mp4"
+        wp_vol = 70
     else:
-        wallpaper_file = "car-camping-in-the-rain-moewalls.com.mp4"
-        wallpaper_volume = 40
+        wp_file = "car-camping-in-the-rain-moewalls.com.mp4"
+        wp_vol = 40
     
-    return wallpaper_file, wallpaper_volume
+    if not VOLUME_ENABLED:
+        wp_vol = 0
+
+    return wp_file, wp_vol
 
 # Function to run the wallpaper changer continuously
 def run_wallpaper_changer():
-
-    global change_based_on_time
-    lively_wallpaper_path_prefix = "C:\\Users\\andre\\OneDrive\\Pictures\\Wallpapers\\"
-
+    
     # set wallpaper and volume based on season
     season = get_current_season()
-    wallpaper_file_name, wallpaper_volume = select_wallpaper(season)
-    wallpaper_path = lively_wallpaper_path_prefix + wallpaper_file_name
-    set_wallpaper(wallpaper_path, 1)
-    set_volume(wallpaper_volume)
+    wp_file, wp_vol = select_wp(season)
+    wp_path = WP_PATH_PREFIX + wp_file
+    set_wp(wp_path)
+    set_vol(wp_vol)
 
     # update brightness of wallpaper based on daylight outside
-    while change_based_on_time:
+    while DYNAMIC_BRIGHTNESS:
         try:          
-            changeProperty("brightness", brightness_level(), 1) # change brightness of wallpaper based on time of day relative to sunrise and sunset for current day
-            time.sleep(900)  # change wallpaper every 15 minutes to account for brightness changes
+            change_property("brightness", calc_new_brightness()) # change brightness of wallpaper based on time of day relative to sunrise and sunset for current day
+            time.sleep(PROP_UPDATE_RATE)  # change wallpaper every 15 minutes to account for brightness changes
         except Exception as e:
             # handle any exceptions that may occur during the process
             print(f"Error: {e}")
-            change_based_on_time = False
-            time.sleep(30) #should wait for key to be pressed instead or something
+            sys.exit()
     
-    if not change_based_on_time:
+    if not DYNAMIC_BRIGHTNESS:
         #set all parameters to default values
         try:          
-            changeProperty("saturation", 0, 1)
-            changeProperty("brightness", 0, 1) 
-            changeProperty("hue", 0, 1)
-            changeProperty("contrast", 0, 1)
-            changeProperty("gamma", 0, 1)
-            changeProperty("speed", 1, 1)
-            exit()
+            change_property("saturation", 0, 1)
+            change_property("brightness", 0, 1) 
+            change_property("hue", 0, 1)
+            change_property("contrast", 0, 1)
+            change_property("gamma", 0, 1)
+            change_property("speed", 1, 1)
         except Exception as e:
             # handle any exceptions that may occur during the process
             print(f"Error: {e}")
-            change_based_on_time = False
-            time.sleep(30) #should wait for key to be pressed instead or something
+            sys.exit()
+    sys.exit()
 
-subprocess.Popen(["C:\\Users\\andre\\AppData\\Local\\Programs\\Lively Wallpaper\\Lively.exe"])
+subprocess.Popen([LIVELY_EXE_PATH])
 time.sleep(3) #ensures Lively is fully opened before executing the rest of the program
 
 run_wallpaper_changer()
